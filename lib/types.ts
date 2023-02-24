7a2687ee9976f2ddaad3ba7ee21aeda78d78a5eb
Deniz <denizdogan@users.noreply.github.com>
2023-02-24 11:58:39 +0100

    Formatting

--- lib/types.ts
+++ lib/types.ts
@@ -6,7 +6,10 @@ export type AsFunction = [str: string]
 /**
  * Parameters of {@link safe} when used as a template tag function.
  */
-export type AsTemplateTag = [strings: TemplateStringsArray, ...values: unknown[]]
+export type AsTemplateTag = [
+  strings: TemplateStringsArray,
+  ...values: unknown[],
+]
 
 /**
  * Parameters that {@link safe} accepts.
