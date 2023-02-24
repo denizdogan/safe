17236112921efffeed0e2fa12736b532100b41e6
Deniz <denizdogan@users.noreply.github.com>
2023-02-24 11:54:59 +0100

    Expose API types as exports

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,12 @@
 /**
  * Parameters of {@link safe} when used as an ordinary function.
  */
-type AsFunction = [str: string]
+export type AsFunction = [str: string]
 
 /**
  * Parameters of {@link safe} when used as a template tag function.
  */
-type AsTemplateTag = [strings: TemplateStringsArray, ...values: unknown[]]
+export type AsTemplateTag = [strings: TemplateStringsArray, ...values: unknown[]]
 
 /**
  * Parameters that {@link safe} accepts.
--- mod.ts
+++ mod.ts
@@ -1,1 +1,2 @@
 export { s, safe } from "./lib/safe.ts"
+export type { AsFunction, AsTemplateTag, SafeParams } from "./lib/types.ts"
